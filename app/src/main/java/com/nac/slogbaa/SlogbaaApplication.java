9f12e61993bc27a770f05f7b30e7ea29ea768345
Shemuel <2060newty@gmail.com>
2026-02-10 22:57:42 +0300

    Enhance SlogbaaApplication configuration with JPA and component scanning
    - Added @ComponentScan, @EntityScan, and @EnableJpaRepositories annotations to configure JPA and component scanning for the application.
    - Improved application structure for better integration with persistence layer.

--- app/src/main/java/com/nac/slogbaa/SlogbaaApplication.java
+++ app/src/main/java/com/nac/slogbaa/SlogbaaApplication.java
@@ -2,8 +2,14 @@ package com.nac.slogbaa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@ComponentScan(basePackages = { "com.nac.slogbaa" })
+@EntityScan(basePackages = { "com.nac.slogbaa.iam.adapters.persistence.entity" })
+@EnableJpaRepositories(basePackages = { "com.nac.slogbaa.iam.adapters.persistence.repository" })
 public class SlogbaaApplication {
 
 	public static void main(String[] args) {
