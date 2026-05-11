0fd5ecffbc199b6b6999cfe8f4896fcdb4b22e2b
Shemuel <2060newty@gmail.com>
2026-05-11 09:10:19 +0300

    cleaned up the home page - removing some sections and temporarily deactivating the statistics section

---
+++ backend/app/src/main/resources/db/migration/V42__drop_videos_news_trainings.sql
@@ -0,0 +1,5 @@
+-- Drop videos, news and in-person training tables as they are removed from the homepage
+
+DROP TABLE IF EXISTS homepage_video;
+DROP TABLE IF EXISTS homepage_news;
+DROP TABLE IF EXISTS in_person_training;
